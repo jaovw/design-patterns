4e4a7da357db7c6a9b0e4ffd4c0c54ebe32dc802
jaovw <joojvw@gmail.com>
2022-12-07 15:01:19 -0300

    class veiculos

---
+++ src/principios/ocp/vehicles/Motorcycle.ts
@@ -0,0 +1,26 @@
+import IVehicleMotorcycle from "./IVehicleMotorcycle"
+
+export default class Motorcycle implements IVehicleMotorcycle {
+
+    constructor(
+        color: string,
+        year: number,
+        engine: number,
+    ) {
+        this.configure(color, year, engine)
+    }
+
+    startVehicle(): void {
+        console.log('Ligando os motores ...')
+    }
+
+    configure(color: string, year: number, engine: number): void {
+
+        console.log(`Criando uma motocicleta:
+        ${color}, ${year}, ${engine} cilindradas.`)
+
+        this.startVehicle()
+    }
+
+
+}
