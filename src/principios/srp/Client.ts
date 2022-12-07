d08c66c61f9c619eeae25696d4d0d58d0701b94e
jaovw <joojvw@gmail.com>
2022-12-07 13:23:04 -0300

    classe client

---
+++ src/principios/srp/Client.ts
@@ -0,0 +1,18 @@
+export default class Client {
+
+    //  UMA UNICA RESPONSABILIDADE - CRUD
+    create(): void {
+
+    }
+    read(): void {
+
+    }
+    update(): void {
+
+    }
+    delete(): void {
+
+    }
+
+    email: string
+}
