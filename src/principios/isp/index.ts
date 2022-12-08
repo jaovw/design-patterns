270c7d1c8bf798d58c1d16023697b7b773bec87a
jaovw <joojvw@gmail.com>
2022-12-08 14:32:20 -0300

    index

---
+++ src/principios/isp/index.ts
@@ -0,0 +1,5 @@
+import Car from "./vehicles/Car";
+import Motorcycle from "./vehicles/Motorcycle";
+
+const carro = new Car('Roxo', 2021, 1.6, 4)
+const motocicleta = new Motorcycle('Preta', 2023, 250)
