1cb4181f305e5a28c78f5eca0f3cc323120f02a2
jaovw <joojvw@gmail.com>
2022-12-13 20:47:27 -0300

    index

---
+++ src/criacional/factoryMethod/index.ts
@@ -0,0 +1,24 @@
+import CarTransport from "./transport/CarTransport"
+import MotorcycleTransport from "./transport/MotorcycleTransport"
+import Transport from "./transport/Transport"
+
+declare var process
+
+let transport: Transport
+
+if (process.argv.includes('--uber')) {
+
+    transport = new CarTransport()
+
+    transport.startTransport()
+
+} else if (process.argv.includes('--log')) {
+
+    transport = new MotorcycleTransport()
+
+    transport.startTransport()
+
+} else {
+
+    console.error('Selecione um tipo de entrega')
+}
