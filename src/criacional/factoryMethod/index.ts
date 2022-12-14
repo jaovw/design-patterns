937fc4218ba97c33ba66fc0a76c3bc17ee42ec7f
jaovw <joojvw@gmail.com>
2022-12-13 21:00:06 -0300

    index

--- src/criacional/factoryMethod/index.ts
+++ src/criacional/factoryMethod/index.ts
@@ -1,3 +1,4 @@
+import BikeTransport from "./transport/BikeTransport"
 import CarTransport from "./transport/CarTransport"
 import MotorcycleTransport from "./transport/MotorcycleTransport"
 import Transport from "./transport/Transport"
@@ -9,15 +10,18 @@ let transport: Transport
 if (process.argv.includes('--uber')) {
 
     transport = new CarTransport()
-
     transport.startTransport()
 
 } else if (process.argv.includes('--log')) {
 
     transport = new MotorcycleTransport()
-
     transport.startTransport()
 
+} else if (process.argv.includes('--eats')) {
+
+    transport = new BikeTransport()
+    transport.startTransport()
+    
 } else {
 
     console.error('Selecione um tipo de entrega')
