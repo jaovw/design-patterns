b0862e89273743377bc0544dac4dfa4bf37936bf
jaovw <joojvw@gmail.com>
2022-12-15 13:33:31 -0300

    novo index com nova chamada utilizando bridge

--- src/estrutural/bridge/index.ts
+++ src/estrutural/bridge/index.ts
@@ -1,5 +1,7 @@
 import IPlatform from "./platforms/IPlatform";
+import Twitch from "./platforms/Twitch";
 import YouTube from "./platforms/YouTube";
+import AdvancedLive from "./transmission/AdvancedLive";
 import Live from "./transmission/Live";
 
 function startLive(platform: IPlatform) {
@@ -12,4 +14,19 @@ function startLive(platform: IPlatform) {
     live.result()
 }
 
-startLive(new YouTube())
+//  REALIZANDO OUTRA PONTE
+function startAdvancedLive(platform: IPlatform) {
+
+    console.log('Aguarde ...')
+
+    const live = new AdvancedLive(platform)
+
+    live.broadcasting()
+    live.result()
+
+    live.subtitles()
+    live.comments()
+}
+
+// startLive(new YouTube())
+startAdvancedLive(new Twitch())
