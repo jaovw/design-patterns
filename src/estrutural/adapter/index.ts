d3892c6ce28dfeb185490285f7aae368d63dbb25
jaovw <joojvw@gmail.com>
2022-12-15 11:50:29 -0300

    novo index

--- src/estrutural/adapter/index.ts
+++ src/estrutural/adapter/index.ts
@@ -1,10 +1,13 @@
+import MercadoPagoAdapter from "./adapters/MercadoPagoAdapter";
 import PayonnerAdapter from "./adapters/PayonnerAdapter";
+import MercadoPago from "./mercadoPago/MercadoPago";
 import Payonner from "./payonner/Payonner";
 import IPayPalPayment from "./paypal/IPaypalPayment";
 import PayPal from "./paypal/PayPal";
 
 // const payment: IPayPalPayment = new PayPal()
-const payment: IPayPalPayment = new PayonnerAdapter(new Payonner())
+// const payment: IPayPalPayment = new PayonnerAdapter(new Payonner())
+const payment: IPayPalPayment = new MercadoPagoAdapter(new MercadoPago)
 
 payment.paypalPayment()
 payment.paypalReceive()
